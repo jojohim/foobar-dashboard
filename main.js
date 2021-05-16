704879d893cfc48fe6bbc92257e0029a6b38106a
jojohim <johanna.himstedt.de@gmail.com>
2021-05-16 11:55:37 +0200

    fetched information for bartenders

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ async function loadJSON(){
 
 function handleData(JSONdata){
 
-
   //HANDLE ORDERS
   const orders = JSONdata.queue
   orders.forEach(displayOrder) //for each order display
@@ -26,6 +25,25 @@ function handleData(JSONdata){
   //HANDLE TAPS
   const taps = JSONdata.taps;
   taps.forEach(makeChartFromTaps);
+
+  //HANDLE BARTENDERS
+  const bartenders = JSONdata.bartenders;
+  bartenders.forEach(displayBartender);
+
+}
+
+function displayBartender(bartender){
+  console.log(bartender);
+  //create clone
+  const copy = document.querySelector("template#bartenderCard").content.cloneNode(true);
+  //populate clone
+  copy.querySelector(".bartenderStatus").textContent = bartender.status;
+  copy.querySelector(".bartenderName").textContent = bartender.name;
+  copy.querySelector(".bartenderServing").textContent = bartender.servingCustomer;
+  //append 
+  document.querySelector("#bartenders").appendChild(copy);
+
+
 }
 
 function displayOrder(order){
@@ -33,9 +51,8 @@ function displayOrder(order){
   //create clone
   const copy = document.querySelector("template#orderCard").content.cloneNode(true);
   //populate clone
-  copy.querySelector(".tableNumber").innerHTML = `Table: ${Math.floor(Math.random() * 5) + 1}`; //random number between 1 & 5
+  copy.querySelector(".tableNumber").textContent = `Table: ${Math.floor(Math.random() * 5) + 1}`; //random number between 1 & 5
   copy.querySelector(".timestamp").textContent = convertTime(order.startTime);
-  console.log(convertTime(order.startTime));
 
   ////////to do: for each type of beer only display once and then show amount
   copy.querySelector(".order").textContent = `${order.order}`;
@@ -51,14 +68,8 @@ function convertTime(epoch){
     return new Date(epoch);
 }
 
-function arrangeOrder(){
-}
-
 function makeChartFromTaps(tap){
-
-  //create new canvas
-  console.log(tap);
-
+/*
 //CREATE COPY
 
 const copy = document.querySelector("template#tapChart").content.cloneNode(true);
@@ -93,5 +104,5 @@ var Tap = new Chart(
 
 //APPEND CHILD
 document.querySelector("#taps").appendChild(copy);
-
+*/
 }
