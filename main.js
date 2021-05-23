ea248f2d9ba20c65d7e2e777ce8d114040265c57
jojohim <johanna.himstedt.de@gmail.com>
2021-05-23 15:58:36 +0200

    added functionality to orders and storage

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-import './sass/styles.scss'
+import "./sass/styles.scss";
+
+const headers = {
+  "Content-Type": "application/json; charset=utf-8",
+  "cache-control": "no-cache",
+};
+
 
 window.addEventListener("DOMContentLoaded", start);
+
+// Pretty arrow function
+// const setEventListeners = () => {
+
+// }
 
 //RESULTS FROM FETCH
 
@@ -8,15 +19,120 @@ let ordersResponseArray = [];
 
 //GLOBAL ARRAYS
 let queueSelected = true;
-let globalQueue =[];
+let globalQueue = [];
 let globalServing = [];
-
+let globalTapLevels = [];
+let globalBartenders = [];
 
 function start() {
   loadJSON();
+  setEventListeners();
 }
 
-async function loadJSON(){
+// Adding all listeners
+function setEventListeners() {
+  setToggleOrdersListeners();
+}
+
+
+// Individual listeners
+function setServeOrderListner(copy) {
+  const button = copy.querySelector(".serveButton");
+
+  button.addEventListener("click", function () {
+    const id = parseInt(button.dataset.id);
+    const element = button.parentElement;
+    let order;
+
+    function getOrder(globalOrder) {
+      if (globalOrder.id === id) {
+        return globalOrder;
+      }
+    }
+
+    if (queueSelected) {
+      if (globalBartenders.some(bartender => bartender.status === "READY")) {
+        order = globalQueue.find(getOrder);
+        const index = globalQueue.findIndex(getOrder);
+        globalQueue.splice(index, 1);
+        globalServing.push(order);
+        updateBartender(order, false);
+        element.remove();
+      }
+    } else {
+      order = globalServing.find(getOrder);
+      const index = globalServing.findIndex(getOrder);
+      globalServing.splice(index, 1);
+      updateTapLevels(order);
+      updateBartender(order, true);
+      element.remove();
+    }
+  });
+}
+
+function setToggleOrdersListeners() {
+  const buttons = document.querySelectorAll(".orderStatusFilter");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      if (!button.classList.contains("selected")) {
+        buttons.forEach(function (button) {
+          button.classList.remove("selected");
+        });
+
+        button.classList.add("selected");
+        queueSelected = !queueSelected;
+      }
+    });
+  });
+}
+
+function updateTapLevels(order) {
+  // Go through each beer in order and update the global 
+  // tap levels if the beer names match
+  order.order.forEach((orderBeer) => {
+    globalTapLevels = globalTapLevels.map(tapBeer => {
+      let newTapBeer = {...tapBeer};
+      if (tapBeer.beer === orderBeer.name) {
+        newTapBeer = {
+          ...newTapBeer,
+          level: newTapBeer.level - orderBeer.amount * 10
+        };
+      } 
+      return newTapBeer;
+    });
+  });
+}
+
+function updateBartender(order, isWorking) {
+  let newBartendersState;
+  if (isWorking) {
+    newBartendersState = globalBartenders.map(bartender => ({
+      ...bartender,
+      servingCustomer: bartender.servingCustomer === order.id ? null : bartender.servingCustomer,
+      status: bartender.servingCustomer === order.id ? 'READY' : bartender.status
+    }));
+  } else {
+    let bartenderFound = false;
+
+    newBartendersState = globalBartenders.map(bartender => {
+      if (bartender.status === 'READY' && !bartenderFound) {
+        bartenderFound = true;
+        return {
+          ...bartender,
+          servingCustomer: order.id,
+          status: 'WORKING'
+        }
+      } else {
+        return bartender;
+      }
+    });
+  }
+
+  globalBartenders = newBartendersState;
+}
+
+async function loadJSON() {
   const dataResponse = await fetch("https://carrotsfoobar.herokuapp.com/");
   const JSONdata = await dataResponse.json();
 
@@ -24,146 +140,188 @@ async function loadJSON(){
   handleData(JSONdata);
 }
 
-function handleData(JSONdata){
-
-  console.log(JSONdata);
-  const orders = JSONdata.queue;
-  const serving = JSONdata.serving;
+function setOrders(data) {
+  const orders = data.queue;
+  const serving = data.serving;
 
   //FOR EACH ORDER SET ATTRIBUTES AND THEN PUSH TO GLOBAL ARRAY
   ////for queue
-  orders.forEach(order => {
+  orders.forEach((order) => {
     const queueItem = getItems(order);
     globalQueue.push(queueItem);
-});
-  ////for serving 
-
-  serving.forEach(serving => {
+  });
+  ////for serving
+  serving.forEach((serving) => {
     const servingItem = getItems(serving);
     globalServing.push(servingItem);
-  })
-  //HANDLE ORDERS
+  });
+}
 
-  //call handle order every few seconds
+function setTapLevels(data) {
+  globalTapLevels = data.taps;
+}
 
-  function handleOrders(){
+function setBartenders(data) {
+  globalBartenders = data.bartenders;
+}
 
+function handleOrders() {
   document.querySelector(".queueFilter").value = `Queue (${globalQueue.length})`;
   document.querySelector(".servingFilter").value = `Serving (${globalServing.length})`;
-  if (queueSelected){
-  globalQueue.forEach(displayOrder);
-  document.querySelector(".servingFilter").classList.remove("active");
-  document.querySelector(".queueFilter").classList.add("active"); //for each order display
-  } else{
+  if (queueSelected) {
     document.querySelector("#orders .orderList").innerHTML = "";
-    globalServing.forEach(displayOrder);
+    globalQueue.forEach((order) => displayOrder(order, true));
+    document.querySelector(".servingFilter").classList.remove("active");
+    document.querySelector(".queueFilter").classList.add("active"); //for each order display
+  } else {
+    document.querySelector("#orders .orderList").innerHTML = "";
+    globalServing.forEach((order) => displayOrder(order, false));
     document.querySelector(".servingFilter").classList.add("active");
     document.querySelector(".queueFilter").classList.remove("active");
   }
 
-  if (orders.length == 0 && queueSelected){
+  if (orders.length == 0 && queueSelected) {
     document.getElementById("noOrdersPlaceholder").classList.remove("hidden");
-  } else{
+  } else {
     document.getElementById("noOrdersPlaceholder").classList.add("hidden");
-  } 
+  }
 }
 
-setInterval(handleOrders, 10000);
-handleOrders();
+function handleBartenders() {
+  document.querySelector("#bartenderCards").innerHTML = "";
+  const bartenders = globalBartenders;
+  console.log({bartenders});
+  bartenders.forEach(displayBartender);
+}
+
+function handleData(JSONdata) {
+  console.log(JSONdata);
+  setOrders(JSONdata);
+  setTapLevels(JSONdata);
+  setBartenders(JSONdata);
+
+  //HANDLE ORDERS
+  //call handle order every few seconds
+  setInterval(handleOrders, 1000);
+  handleOrders();
 
   //HANDLE TAPS
-  const taps = JSONdata.taps;
-  taps.forEach(makeChartFromTaps);
+  globalTapLevels.forEach(makeChartFromTaps);
 
-  function handleBartenders(){
-    document.querySelector("#bartenderCards").innerHTML = "";
-  const bartenders = JSONdata.bartenders;
-  bartenders.forEach(displayBartender);
-  }
-  setInterval(handleBartenders, 5000)
-handleBartenders();
+  // HANDLE BARTENDERS
+  setInterval(handleBartenders, 1000);
+  handleBartenders();
 
-//HANDLE KEG STORAGE
-const kegs = JSONdata.storage
-kegs.forEach(displayKegStorage);
+  //HANDLE KEG STORAGE
+  const kegs = JSONdata.storage;
+  kegs.forEach(displayKegStorage);
 }
 
-function getItems(order){
-
+function getItems(order) {
   //call clean order to clean up order
+  const uniqueArray = [...new Set(order.order)];
+  const parsedOrder = uniqueArray.map(item => ({
+    name: item,
+    amount: order.order.filter(order => order === item).length
+  }));
+
   return {
+    id: order.id,
     timestamp: convertTime(order.startTime),
-    order: order.order,
+    order: parsedOrder,
     tableNumber: Math.floor(Math.random() * 5) + 1,
     total: order.order.length,
-  }
+  };
 }
 
-
-function displayBartender(bartender){
+function displayBartender(bartender) {
   //create clone
-  const copy = document.querySelector("template.bartenderCard").content.cloneNode(true);
+  const copy = document
+    .querySelector("template.bartenderCard")
+    .content.cloneNode(true);
   //populate clone
+
+  console.log({bartender});
+
   copy.querySelector(".bartenderStatus").textContent = bartender.status;
   copy.querySelector(".bartenderName").textContent = bartender.name;
   copy.querySelector(".bartenderPhoto").src = "user.svg";
 
-  /////change colour of status 
+  /////change colour of status
   copy.querySelector(".bartenderStatus").style.color = checkColorForStatus();
 
-  function checkColorForStatus(){
-if(bartender.status == "WORKING"){
-  return "rgba(88,221,107,1.0)";
-} else{
-  return "rgba(229,186,88,1.0)";
-}
+  function checkColorForStatus() {
+    if (bartender.status == "WORKING") {
+      return "rgba(88,221,107,1.0)";
+    } else {
+      return "rgba(229,186,88,1.0)";
+    }
   }
-  //append 
+  //append
   document.querySelector("#bartenderCards").appendChild(copy);
 }
 
-function displayOrder(order){
-
+function displayOrder(order, isQueue) {
   //create clone
-  const copy = document.querySelector("template#orderCard").content.cloneNode(true);
+  let copy;
+  if (isQueue) {
+    copy = document
+      .querySelector("template#orderCardQueue")
+      .content.cloneNode(true);
+  } else {
+    copy = document
+      .querySelector("template#orderCardServe")
+      .content.cloneNode(true);
+  }
+
   //populate clone
   copy.querySelector(".tableNumber").textContent = `Table: ${order.tableNumber}`; //random number between 1 & 5
   copy.querySelector(".timestamp").textContent = order.timestamp;
-
   ////////to do: for each type of beer only display once and then show amount
-  copy.querySelector(".order").textContent = `${order.order}`;
 
+  const ul = document.createElement('ul');
+  order.order.forEach(order => {
+    const li = document.createElement('li');
+    li.textContent = `${order.name} ...... ${order.amount}`;
+    ul.appendChild(li);
+  })
+
+  copy.querySelector(".order").appendChild(ul);
   copy.querySelector(".orderTotal").textContent = `Total: ${order.total}`;
-  //append 
-  document.querySelector("#orders .orderList").appendChild(copy);
+  copy.querySelector(".serveButton").dataset.id = order.id;
 
+  //append
+  setServeOrderListner(copy);
+  document.querySelector("#orders .orderList").appendChild(copy);
 }
 
 //DISPLAY CURRENT TIME IN CORNER
-function getCurrentTime(){
-  document.querySelector(".dateTime").textContent = convertTime(new Date);
+function getCurrentTime() {
+  document.querySelector(".dateTime").textContent = convertTime(new Date());
 }
 
 setInterval(getCurrentTime, 1000);
 getCurrentTime();
 
-function convertTime(epoch){   
-  ///////to do: make time look pretty 
+function convertTime(epoch) {
+  ///////to do: make time look pretty
   const time = new Date(epoch);
-  const dd = String(time.getDate()).padStart(2, '0'); 
-  const mm = String(time.getMonth() + 1).padStart(2, '0');
+  const dd = String(time.getDate()).padStart(2, "0");
+  const mm = String(time.getMonth() + 1).padStart(2, "0");
   const year = time.getFullYear();
   const hours = time.getHours();
-  const minutes = String(time.getMinutes()).padStart(2, '0');
-  const editedTime = `${dd}-${mm}-${year}, ${hours}:${minutes}`
+  const minutes = String(time.getMinutes()).padStart(2, "0");
+  const editedTime = `${dd}-${mm}-${year}, ${hours}:${minutes}`;
 
   return editedTime;
 }
 
-function displayKegStorage(keg){
-  console.log(keg)
+function displayKegStorage(keg) {
+  console.log(keg);
   //CREATE COPY
-  const copy = document.querySelector("template#kegStorage").content.cloneNode(true);
+  const copy = document
+    .querySelector("template#kegStorage")
+    .content.cloneNode(true);
 
   //POPULATE
   copy.querySelector(".kegName").textContent = keg.name;
@@ -172,65 +330,66 @@ function displayKegStorage(keg){
   //APPEND
   document.getElementById("kegs").appendChild(copy);
 }
+
+function getChartConfig(beer, tapLevelInPints) {
+  const data = {
+    datasets: [
+      {
+        label: beer,
+        data: [tapLevelInPints, 250 - tapLevelInPints],
+        backgroundColor: [getColorForChart(tapLevelInPints), "transparent"],
+        borderWidth: 1,
+        borderColor: "#E4E0FF",
+      },
+    ],
+  };
+
+  const config = {
+    type: "doughnut",
+    data: data,
+    options: {
+      cutout: "75%",
+    },
+  };
+
+  return config;
+}
+
 function makeChartFromTaps(tap) {
+  //CREATE COPY
+  const copy = document
+    .querySelector("template#tapChart")
+    .content.cloneNode(true);
+  //give id to each
+  copy.querySelector(".tap").dataset.id = tap.id;
+  //POPULATE COPY
 
- //CREATE COPY
- 
- const copy = document.querySelector("template#tapChart").content.cloneNode(true);
- //give id to each
- copy.querySelector(".tap").dataset.id = tap.id;
- //POPULATE COPY
-
- ///FOR LABELLING
-
- const tapLevelInPints = tap.level/10;
- copy.querySelector(".tapName").textContent = tap.beer;
- copy.querySelector(".tapAmount").textContent = `${tapLevelInPints} Pints`;
- ///FOR CHART
- /////setup
- 
- const data = {
- datasets: [{
- label: tap.beer,
- data: [tapLevelInPints, 250-tapLevelInPints],
- backgroundColor: [getColorForChart(), "transparent"],
- borderWidth: 1,
- borderColor: "#E4E0FF",
- }],
- };
+  ///FOR LABELLING
+  const tapLevelInPints = tap.level / 10;
+  copy.querySelector(".tapName").textContent = tap.beer;
+  copy.querySelector(".tapAmount").textContent = `${tapLevelInPints} Pints`;
 
 
-  function getColorForChart(){
-    if(tapLevelInPints >= 100){
-      return "rgba(88,221,107,1.0)";
-    } else if(tapLevelInPints < 100 && tapLevelInPints >= 50){
-      return "rgba(229,186,88,1.0)";
-    } else if(tapLevelInPints < 50){
-      return "rgba(221,114,88,1.0)"};
+  ///FOR CHART
+  //////config
+  const config = getChartConfig(tap.beer, tapLevelInPints); 
+
+  //APPEND CHILD
+  document.querySelector("#taps").appendChild(copy);
+
+  /////render
+  var tapChart = new Chart(
+    document.querySelector(`.tap[data-id="${tap.id}"]`),
+    config
+  );
+}
+
+function getColorForChart(level) {
+  if (level >= 100) {
+    return "rgba(88,221,107,1.0)";
+  } else if (level < 100 && level >= 50) {
+    return "rgba(229,186,88,1.0)";
+  } else if (level < 50) {
+    return "rgba(221,114,88,1.0)";
   }
-
- //////config
- 
- const config = {
- type: "doughnut",
- data: data,
- options: {
-  cutout: "75%",
- }
- };
-
- //APPEND CHILD
- document.querySelector("#taps").appendChild(copy);
- 
- /////render
- 
- var Tap = new Chart(
- document.querySelector(`.tap[data-id="${tap.id}"]`),
- config
- );
- }
-
-
-
-
-
+}
